000983ad5324dfda015a9d8904936783b0b56011
alex <alexander.o.schenk@gmail.com>
2025-02-18 23:20:01 +0100

    refactor pixel shading fct

--- src/5_ray_render.c
+++ src/5_ray_render.c
@@ -6,7 +6,7 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:47:07 by aschenk           #+#    #+#             */
-/*   Updated: 2025/02/18 22:46:59 by aschenk          ###   ########.fr       */
+/*   Updated: 2025/02/18 23:16:46 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,35 +24,62 @@ void	render_scene(t_rt *rt);
 
 /**
 Traces the camera ray and finds the intersection
- @param rt 	Pointer to the main structure of the program.
- @param x 	The x-coordinate of the pixel.
- @param y 	The y-coordinate of the pixel.
+ @param rt 				Pointer to the main structure.
+ @param x 				The x-coordinate of the pixel.
+ @param y 				The y-coordinate of the pixel.
  @param camera_ray_ix 	Pointer to the intersection structure to be filled.
 
- @return	`1` if an intersection was found, `0` otherwise.
+ @return				`1` if an intersection was found, `0` otherwise.
 */
-// static int	trace_camera_ray(t_rt *rt, int x, int y, t_ix *camera_ray_ix)
-// {
-// 	t_vec3	camera_ray_dir;
+static int	trace_camera_ray(t_rt *rt, int x, int y, t_ix *camera_ray_ix)
+{
+	t_vec3	camera_ray_dir;
 
-// 	camera_ray_dir = compute_camera_ray(x, y, rt->scene.cam);
-// 	find_ix(rt->scene.cam.pos, camera_ray_dir, rt, camera_ray_ix);
-// 	return (camera_ray_ix->hit_obj != NULL);
-// }
+	camera_ray_dir = compute_camera_ray(x, y, rt->scene.cam);
+	find_ix(rt->scene.cam.pos, camera_ray_dir, rt, camera_ray_ix);
+	return (camera_ray_ix->hit_obj != NULL);
+}
 
 /**
 Handles the shadow ray calculation and checks for light obstruction
+ @param rt 				Pointer to the main structure.
+ @param camera_ray_ix 	Pointer to the intersection structure of the camera ray.
 
+ @return				`1` if the shadow ray is obstructed, `0` otherwise.
 */
-// static int	handle_shadow_ray(t_rt *rt, t_ix *camera_ray_ix)
-// {
-// 	t_shdw	shadow_ray;
-// 	t_ix	shadow_ray_ix;
+static int	handle_shadow_ray(t_rt *rt, t_ix *camera_ray_ix)
+{
+	t_shdw	shadow_ray;
+	t_ix	shadow_ray_ix;
 
-// 	shadow_ray = compute_shadow_ray(camera_ray_ix, rt->scene.light);
-// 	find_ix(shadow_ray.ori, shadow_ray.dir, rt, &shadow_ray_ix);
-// 	return (shadow_ray_ix.hit_obj && shadow_ray_ix.t_hit < shadow_ray.len);
-// }
+	shadow_ray = compute_shadow_ray(camera_ray_ix, rt->scene.light);
+	find_ix(shadow_ray.ori, shadow_ray.dir, rt, &shadow_ray_ix);
+	return (shadow_ray_ix.hit_obj && shadow_ray_ix.t_hit < shadow_ray.len);
+}
+
+/**
+Computes and applies the final color for a pixel based shadows, reflections,
+and lighting.
+ - Determines if the point is in shadow.
+ - Will eventually incorporate reflection and lighting calculations.
+ - Sets the computed color to the pixel.
+ @param rt				Pointer to the main structure.
+ @param x				The pixel's x-coordinate.
+ @param y				The pixel's y-coordinate.
+ @param camera_ray_ix	Pointer to the intersection data of the camera ray.
+*/
+static void	shade_pixel(t_rt *rt, int x, int y, t_ix *camera_ray_ix)
+{
+	int	color;
+	int	is_shadowed;
+
+	is_shadowed = handle_shadow_ray(rt, camera_ray_ix);
+	if (is_shadowed)
+		color = 0x000000; // actually it's obj_color in ambient light
+	else
+		color = color_to_hex(camera_ray_ix->hit_obj->color);
+	set_pixel_color(&rt->mlx.img, x, y, color);
+}
 
 /**
 Render a single pixel on the screen by computing the ray direction and finding
@@ -64,28 +91,14 @@ the closest intersection.
 */
 static void	render_pixel(t_rt *rt, int x, int y)
 {
-	t_vec3	camera_ray_dir;
 	t_ix	camera_ray_ix;
-	t_shdw	shadow_ray;
-	t_ix	shadow_ray_ix;
 
-	camera_ray_dir = compute_camera_ray(x, y, rt->scene.cam);
-	find_ix(rt->scene.cam.pos, camera_ray_dir, rt, &camera_ray_ix);
-	if (!camera_ray_ix.hit_obj)
+	if (trace_camera_ray(rt, x, y, &camera_ray_ix) == 0)
 	{
 		set_pixel_color(&rt->mlx.img, x, y, BG_COLOR);
 		return ;
 	}
-	shadow_ray = compute_shadow_ray(&camera_ray_ix, rt->scene.light);
-	find_ix(shadow_ray.ori, shadow_ray.dir, rt, &shadow_ray_ix);
-	if (shadow_ray_ix.hit_obj && shadow_ray_ix.t_hit < shadow_ray.len)
-		set_pixel_color(&rt->mlx.img, x, y, 0x000000); // actually it's obj_color in ambient light
-	else
-		set_pixel_color(&rt->mlx.img, x, y,
-			color_to_hex(camera_ray_ix.hit_obj->color));
-
-	// compute_color(camera_ray_dir, rt, &camera_ray_ix);
-	// set_pixel_color(&rt->mlx.img, x, y, camera_ray_ix.ixn_color);
+	shade_pixel(rt, x, y, &camera_ray_ix);
 }
 
 /**
